c4195a72c256f79d40aa7fbbf96412266a1e4987
nam <nam.rzedziak@wp.pl>
2022-05-19 00:12:45 +0200

    Adding lizard and spock to rps

--- rps.py
+++ rps.py
@@ -1,7 +1,9 @@
 combinations = {
-    1 : 3,
-    2 : 1,
-    3 : 2
+    1 : [3,4],
+    2 : [1,5],
+    3 : [2,4],
+    4 : [2,5],
+    5 : [3,1]
 }
 while True:
     game = input("Start or stop game?: ").lower()
@@ -11,21 +13,20 @@ while True:
         points1 = 0
         points2 = 0
         while True:
-            p1 = int(input("PLAYER 1 Choose rock , paper or scissors " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors:  "))
+            p1 = int(input("PLAYER 1 Choose rock , paper, scissors, lizard or spock " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors, 4 - Lizard, 5 - Spock:  "))
             while p1 not in combinations:
                 print("wrong input")
                 p1 = int(
-                    input("PLAYER 1 Choose rock , paper or scissors " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors:  "))
-            p2 = int(input("PLAYER 2 Choose rock , paper or scissors " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors:  "))
+                    input("PLAYER 1 Choose rock , paper, scissors, lizard or spock " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors, 4 - Lizard, 5 - Spock:  "))
+            p2 = int(input("PLAYER 2 Choose rock , paper, scissors, lizard or spock " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors, 4 - Lizard, 5 - Spock:  "))
             while p2 not in combinations:
                 print("wrong input")
                 p2 = int(
-                    input("PLAYER 2 Choose rock , paper or scissors " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors:  "))
-            if combinations[p1] == p2:
+                    input("PLAYER 2 Choose rock , paper, scissors, lizard or spock " + "\n" + "1 - Rock, 2 - Paper, 3 - Scissors, 4 - Lizard, 5 - Spock:  "))
+            if p2 in combinations[p1]:
                 print("p1 won")
                 points1 += 1
-
-            elif combinations[p2] == p1:
+            elif p1 in combinations[p2]:
                 print("p2 won")
                 points2 += 1
             elif p2 == p1:
